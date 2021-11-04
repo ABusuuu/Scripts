a815d60d9da71818dec98cdb3a535e708c46f573
ABusuuu <91973755+ABusuuu@users.noreply.github.com>
2021-11-04 09:04:04 +0100

    Create rdp.js

---
+++ TightVNC via HTML/rdp.js
@@ -0,0 +1,9 @@
+var destination=(WScript.Arguments(0))
+var search='bmsvnc://'
+var rdpexe="C:\\Program Files\\TightVNC\\tvnviewer.exe"
+//WScript.Echo(destination)
+destination=destination.replace(search, '')
+destination=destination.replace('/', '')
+var ws = new ActiveXObject("WScript.Shell")
+//WScript.Echo(rdpexe +" -host="+destination +" -port=5900" +" -password=")
+ws.Exec(rdpexe +" -host="+destination +" -port=5900" +" -password=")
