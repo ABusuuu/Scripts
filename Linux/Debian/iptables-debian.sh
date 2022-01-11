e8520cebf18d6029838c22ba6ca712c3c8ba67a8
ABusuuu <91973755+ABusuuu@users.noreply.github.com>
2022-01-11 16:38:18 +0100

    Update iptables-debian.sh

--- Linux/Debian/iptables-debian.sh
+++ Linux/Debian/iptables-debian.sh
@@ -1,19 +1,41 @@
 #!/bin/bash
 
 #apt-get install iptables iptables-persistent
+
+#### FLUSH ####
+
 iptables -F
+
+#### SSH ####
+
 iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+
+#### LO ####
+
+iptables -A OUTPUT -o lo -j ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
+
+
 #iptables -A INPUT -p tcp -m multiport --dport 80,443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 #iptables -A OUTPUT -p tcp -m multiport --sport 80,443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 #iptables -A INPUT -p udp -m udp --dport 53 -j ACCEPT
 #iptables -A OUTPUT -p udp -m udp --sport 53 -j ACCEPT
 #iptables -A INPUT -p tcp -m tcp --dport 53 -j ACCEPT
 #iptables -A OUTPUT -p tcp -m tcp --sport 53 -j ACCEPT
+
+#### BLOCK ALL #####
+
 iptables -P INPUT DROP
 iptables -P OUTPUT DROP
 iptables -P FORWARD DROP
+
+##### IPv6 ACCEPT #####
+
 ip6tables -P INPUT ACCEPT
 ip6tables -P OUTPUT ACCEPT
 ip6tables -P FORWARD ACCEPT
+
+##### SAVE #####
+
 netfilter-persistent save
